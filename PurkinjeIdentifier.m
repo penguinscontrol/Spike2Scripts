94f3339b41df0e3a2526f9b79b3c8e49879bc888
Radu Darie <penguinscontrol@gmail.com>
2013-06-14 18:02:51 -0400

    Purkinje Finder 3

--- PurkinjeIdentifier.m
+++ PurkinjeIdentifier.m
@@ -1,4 +1,6 @@
+clear;clc;
 cd('E:\\Spike_Sorting\\spike2temp\\');
+
 load('postss.mat'); % post simple spike data
 
 tn1 = whos; % temporary name holder
@@ -6,4 +8,11 @@ tn2 = tn1.name;
 
 eval(['datastr = ', tn2,';']); % get data as a struct
 data = datastr.values;
-plot(data');
+data = data';
+
+
+data_fft = fft(data);
+[rws,cls] = size(data_fft);
+data_fft_amp = abs(data_fft(2:(floor(rws/2)+1),:)); %get fft amplitudes, minus DC offset
+
+data_fft_amp_maxes = max(data_fft_amp);
