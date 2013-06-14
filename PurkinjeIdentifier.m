37ff563ebeb85ff1f8bbf7b18f9f619ad007d3f8
Radu Darie <penguinscontrol@gmail.com>
2013-06-14 18:20:45 -0400

    Purkinje Finder 4

--- PurkinjeIdentifier.m
+++ PurkinjeIdentifier.m
@@ -15,4 +15,9 @@ data_fft = fft(data);
 [rws,cls] = size(data_fft);
 data_fft_amp = abs(data_fft(2:(floor(rws/2)+1),:)); %get fft amplitudes, minus DC offset
 
-data_fft_amp_maxes = max(data_fft_amp);
+data_fft_maxes = max(data_fft_amp);
+[count_maxes,bin_centers] = hist(data_fft_maxes,1000);
+plot(bin_centers,count_maxes,'ko');
+
+newlabels = ones(1,cls);
+newlabels(data_fft_maxes > 40) = 30;
