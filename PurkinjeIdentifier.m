44d23e50e6cd3cbfc2ce67dc65c465c0800916c9
Radu Darie <penguinscontrol@gmail.com>
2013-06-18 10:41:58 -0400

    Purkinje Finder 5

--- PurkinjeIdentifier.m
+++ PurkinjeIdentifier.m
@@ -1,7 +1,10 @@
-clear;clc;
+
 cd('E:\\Spike_Sorting\\spike2temp\\');
 
 load('postss.mat'); % post simple spike data
+
+%ssclus = 1;
+%timelength = 1377;
 
 tn1 = whos; % temporary name holder
 tn2 = tn1.name;
@@ -10,14 +13,28 @@ eval(['datastr = ', tn2,';']); % get data as a struct
 data = datastr.values;
 data = data';
 
-
 data_fft = fft(data);
 [rws,cls] = size(data_fft);
 data_fft_amp = abs(data_fft(2:(floor(rws/2)+1),:)); %get fft amplitudes, minus DC offset
 
 data_fft_maxes = max(data_fft_amp);
 [count_maxes,bin_centers] = hist(data_fft_maxes,1000);
-plot(bin_centers,count_maxes,'ko');
 
-newlabels = ones(1,cls);
-newlabels(data_fft_maxes > 40) = 30;
+timelength = round(timelength);
+cutoff = sum(count_maxes)-timelength;
+
+cumul_hist = cumsum(count_maxes);
+
+figure(1);
+plot(bin_centers(count_maxes ~= 0),count_maxes(count_maxes ~= 0),'ko');
+hold on;
+plot(bin_centers(cumul_hist>cutoff),count_maxes(cumul_hist>cutoff),'r+');
+
+fft_cutoff = bin_centers(cumul_hist>cutoff);
+fft_cutoff = fft_cutoff(1);
+
+mynewlabels = ones(1,cls).*double(ssclus);
+
+mynewlabels(data_fft_maxes >= fft_cutoff) = ssclus+30;
+
+text(bin_centers(count_maxes == max(count_maxes)),count_maxes(count_maxes == max(count_maxes)),sprintf('This is working!'));
