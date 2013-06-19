0b3515eadc6f59a0aab9bec69d43d470a6b84d69
Radu Darie <penguinscontrol@gmail.com>
2013-06-19 12:41:57 -0400

    Purkinje Finder 6

--- PurkinjeIdentifier.m
+++ PurkinjeIdentifier.m
@@ -1,10 +1,11 @@
-
 cd('E:\\Spike_Sorting\\spike2temp\\');
 
 load('postss.mat'); % post simple spike data
 
-%ssclus = 1;
-%timelength = 1377;
+% Variables from Spike2, uncomment for debugging
+% ssclus = 1;
+% timelength = 1377;
+% tsamp = 5e-6;
 
 tn1 = whos; % temporary name holder
 tn2 = tn1.name;
@@ -13,28 +14,75 @@ eval(['datastr = ', tn2,';']); % get data as a struct
 data = datastr.values;
 data = data';
 
+Fsamp = tsamp^(-1);
 data_fft = fft(data);
 [rws,cls] = size(data_fft);
-data_fft_amp = abs(data_fft(2:(floor(rws/2)+1),:)); %get fft amplitudes, minus DC offset
 
-data_fft_maxes = max(data_fft_amp);
-[count_maxes,bin_centers] = hist(data_fft_maxes,1000);
-
-timelength = round(timelength);
-cutoff = sum(count_maxes)-timelength;
-
-cumul_hist = cumsum(count_maxes);
-
-figure(1);
-plot(bin_centers(count_maxes ~= 0),count_maxes(count_maxes ~= 0),'ko');
-hold on;
-plot(bin_centers(cumul_hist>cutoff),count_maxes(cumul_hist>cutoff),'r+');
-
-fft_cutoff = bin_centers(cumul_hist>cutoff);
-fft_cutoff = fft_cutoff(1);
 
 mynewlabels = ones(1,cls).*double(ssclus);
+        
+whatttodo = 'fft_max_3sd';
+switch whatttodo
+    case 'fft_max_1hz'
+        
+        data_fft_amp = abs(data_fft(1:(floor(rws/2)+1),:)); %get fft amplitudes
+        
+        freqs = linspace(0,Fsamp/2,(floor(rws/2)+1));
+        llim = find(freqs > 0); llim = llim(1);
+        rlim = find(freqs < 5000); rlim = rlim(end);
+        
+        data_fft_amp = data_fft_amp(llim:rlim,:);
+        
+        data_fft_maxes = max(data_fft_amp);
+        [count_maxes,bin_centers] = hist(data_fft_maxes,1000);
 
-mynewlabels(data_fft_maxes >= fft_cutoff) = ssclus+30;
+        timelength = round(timelength);
+        cutoff = sum(count_maxes)-timelength;
 
-text(bin_centers(count_maxes == max(count_maxes)),count_maxes(count_maxes == max(count_maxes)),sprintf('This is working!'));
+        cumul_hist = cumsum(count_maxes);
+
+%       figure(1);
+%       plot(bin_centers(count_maxes ~= 0),count_maxes(count_maxes ~= 0),'ko');
+%       hold on;
+%       plot(bin_centers(cumul_hist>cutoff),count_maxes(cumul_hist>cutoff),'r+');
+
+        fft_cutoff = bin_centers(cumul_hist>cutoff);
+        fft_cutoff = fft_cutoff(1);
+        
+        mynewlabels(data_fft_maxes >= fft_cutoff) = ssclus+30;
+    
+    case 'fft_sum_3sd'
+        
+        data_fft_amp = abs(data_fft(1:(floor(rws/2)+1),:)); %get fft amplitudes
+        
+        freqs = linspace(0,Fsamp/2,(floor(rws/2)+1));
+        llim = find(freqs > 0); llim = llim(1);
+        rlim = find(freqs < 4000); rlim = rlim(end);
+        
+        data_fft_amp = data_fft_amp(llim:rlim,:);
+        
+        data_fft_sum = sum(data_fft_amp);
+        
+        [muhat,sigmahat] = normfit(data_fft_sum');
+        cutoff = muhat+3*sigmahat;
+        
+        mynewlabels(data_fft_sum >= cutoff) = ssclus+30;
+        
+    otherwise %fft_max_3sd
+        
+        data_fft_amp = abs(data_fft(1:(floor(rws/2)+1),:)); %get fft amplitudes
+        
+        freqs = linspace(0,Fsamp/2,(floor(rws/2)+1));
+        llim = find(freqs > 0); llim = llim(1);
+        rlim = find(freqs < 4000); rlim = rlim(end);
+        
+        data_fft_amp = data_fft_amp(llim:rlim,:);
+        
+        data_fft_maxes = max(data_fft_amp);
+        
+        [muhat,sigmahat] = normfit(data_fft_maxes');
+        cutoff = muhat+3*sigmahat;
+        
+        mynewlabels(data_fft_maxes >= cutoff) = ssclus+30;
+        
+end
