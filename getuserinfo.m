e6327bcee2e125bbcedff38eefd67ca5b219ea2a
Radu Darie <penguinscontrol@gmail.com>
2013-06-26 14:24:28 -0400

    minor changes
    everything seems to be working all right

---
+++ getuserinfo.m
@@ -0,0 +1,28 @@
+archst  = computer('arch');
+
+if strcmp(archst, 'maci64')
+    name = getenv('USER');
+    if strcmp(name, 'nick')
+        directory = '/Users/nick/Dropbox/filesforNick/';
+    elseif strcmp(name, 'Frank')
+        directory = '/Users/Frank/Desktop/monkeylab/data/';
+    elseif strcmp(name, 'zacharyabzug')
+        directory = '/Users/zacharyabzug/Desktop/zackdata/';
+    end
+    slash = '/';
+elseif strcmp(archst, 'win32') || strcmp(archst, 'win64')
+    if strcmp(getenv('username'),'SommerVD') || ...
+            strcmp(getenv('username'),'LabV') || ...
+            strcmp(getenv('username'),'Purkinje') || ...
+            strcmp(getenv('username'),'vp35')
+        directory = 'C:\Data\Recordings\';
+    elseif strcmp(getenv('username'),'DangerZone')
+        directory = 'E:\data\Recordings\';
+    elseif strcmp(getenv('username'),'Radu')
+        directory = 'E:\Spike_Sorting\';
+        githubdirectory = 'C:\Users\Radu\Documents\GitHub\';
+    else
+        directory = 'B:\data\Recordings\';
+    end
+    slash = '\';
+end
