eb25a3f9a9e40da58fdea4c9acb672ea66be415f
penguinscontrol <penguinscontrol@gmail.com>
2013-10-16 11:44:37 -0400

    update plotting

--- Cell_Identifier/load_training_set.m
+++ Cell_Identifier/load_training_set.m
@@ -1,11 +1,10 @@
 function tset = load_training_set()
 getuserinfo; % which computer am I on
 cd([githubdirectory 'Spike2Scripts\Cell_Identifier\training_set\']);
-
+tset = [];
 tsetls = ls;
 tsetls = tsetls(3:end,:);
 [nofiles, ~] = size(tsetls);
-tset = cell(nofiles,1);
 c = 1;
 for a = 1:nofiles
     cur = load(tsetls(a,:));
@@ -14,7 +13,7 @@ for a = 1:nofiles
     [noclus, ~] = size(names);
     for b = 1:noclus
         if isa(eval(['cur.',names{b}]),'unit')
-            eval(['tset{', num2str(c),'}= cur.', names{b},';']);
+            eval(['tset(', num2str(c),')= cur.', names{b},';']);
             c = c+1;
         end
     end   
--- Cell_Identifier/plotfeats.m
+++ Cell_Identifier/plotfeats.m
@@ -1,7 +1,7 @@
-function plotfeats2d( units, feat1, feat2, feat3 )
+function plotfeats( units, feat1, feat2, feat3 )
 % plots the contents of cell array (items of the type unit)
 if nargin == 2
-if isprop(units{1,1},feat1)
+if isprop(units(1,1),feat1)
     figure();
     [r, c] = size(units);
     hold all;
@@ -9,31 +9,46 @@ if isprop(units{1,1},feat1)
     unc = [];
     gol = [];
     den = [];
+    ub = [];
+    bas = [];
+    ste = [];
+    gra = [];
+    bor = [];
     pur = [];
     fib = [];
     
-    ha = zeros(1,5);
+    ha = zeros(1,10);
     for a = 1:r
-        switch units{a}.label
+        switch units(a).label
             case 'unclassified'
-                unc = [unc units{a}.(feat1)];
-            case 'Golgi'
-                gol = [den units{a}.(feat1)];               
-            case 'Dentate'
-                den = [den units{a}.(feat1)];           
-            case 'Purkinje'
-                pur = [pur units{a}.(feat1)];              
-            case 'Fiber'
-                fib = [fib units{a}.(feat1)];           
+                unc = [unc units(a).(feat1)];
+            case 'golgi'
+                gol = [gol units(a).(feat1)];               
+            case 'dentate'
+                den = [den units(a).(feat1)];           
+            case 'purkinje'
+                pur = [pur units(a).(feat1)];              
+            case 'fiber'
+                fib = [fib units(a).(feat1)];              
+            case 'ub'
+                ub = [ub units(a).(feat1)];              
+            case 'basket'
+                bas = [bas units(a).(feat1)];              
+            case 'stellate'
+                ste = [ste units(a).(feat1)];              
+            case 'granule'
+                gra = [gra units(a).(feat1)];              
+            case 'border'
+                bor = [bor units(a).(feat1)];                
         end        
     end
     
-    his = cell(1,5);
-    hisbins = cell(1,5);
+    his = cell(1,10);
+    hisbins = cell(1,10);
     
     if ~isempty(unc)
         [his{1},hisbins{1}] = hist(unc,1000);
-        ha(1) = stem(hisbins{1}(his{1}~= 0),his{1}(his{1}~= 0),'ko','DisplayName','unclassified');
+        ha(1) = stem(hisbins{1}(his{1}~= 0),his{1}(his{1}~= 0),'ko','DisplayName','Unclassified');
     end
     if ~isempty(gol)
         [his{2},hisbins{2}] = hist(gol,1000);
@@ -51,6 +66,26 @@ if isprop(units{1,1},feat1)
         [his{5},hisbins{5}] = hist(fib,1000);
         ha(5) = stem(hisbins{5}(his{5}~= 0),his{5}(his{5}~= 0),'m+','DisplayName','Fiber');
     end
+    if ~isempty(ub)
+        [his{6},hisbins{6}] = hist(ub,1000);
+        ha(6) = stem(hisbins{6}(his{6}~= 0),his{6}(his{6}~= 0),'m+','DisplayName','Unipolar Brush');
+    end
+    if ~isempty(bas)
+        [his{7},hisbins{7}] = hist(bas,1000);
+        ha(7) = stem(hisbins{7}(his{7}~= 0),his{7}(his{7}~= 0),'m+','DisplayName','Basket');
+    end
+    if ~isempty(ste)
+        [his{8},hisbins{8}] = hist(ste,1000);
+        ha(8) = stem(hisbins{8}(his{8}~= 0),his{8}(his{8}~= 0),'m+','DisplayName','Stellate');
+    end
+    if ~isempty(gra)
+        [his{9},hisbins{9}] = hist(gra,1000);
+        ha(9) = stem(hisbins{9}(his{9}~= 0),his{9}(his{9}~= 0),'m+','DisplayName','Granule');
+    end
+    if ~isempty(bor)
+        [his{10},hisbins{10}] = hist(bor,1000);
+        ha(10) = stem(hisbins{10}(his{10}~= 0),his{10}(his{10}~= 0),'m+','DisplayName','Border');
+    end
      
     
     xlabel(feat1);
@@ -62,44 +97,74 @@ else
 end    
 elseif nargin == 3
 
-if isprop(units{1,1},feat1)&&isprop(units{1,1},feat2)
+if isprop(units(1,1),feat1)&&isprop(units(1,1),feat2)
     figure();
     [r, c] = size(units);
     hold all;
     
-    ha = zeros(1,5);
+    ha = zeros(1,10);
     for a = 1:r
-        if ~isempty(units{a})
-        switch units{a}.label
+        if ~isempty(units(a))
+        switch units(a).label
             case 'unclassified'
                 if ha(1) == 0
-                    ha(1) = plot(units{a}.(feat1),units{a}.(feat2),'ko','DisplayName','unclassified');
+                    ha(1) = plot(units(a).(feat1),units(a).(feat2),'ko','DisplayName','Unclassified');
                 else
-                    plot(units{a}.(feat1),units{a}.(feat2),'ko');
+                    plot(units(a).(feat1),units(a).(feat2),'ko');
                 end
-            case 'Golgi'
+            case 'golgi'
                 if ha(2) == 0
-                    ha(2) = plot(units{a}.(feat1),units{a}.(feat2),'rs','DisplayName','Golgi');
+                    ha(2) = plot(units(a).(feat1),units(a).(feat2),'rs','DisplayName','Golgi');
                 else
-                    plot(units{a}.(feat1),units{a}.(feat2),'rs');
+                    plot(units(a).(feat1),units(a).(feat2),'rs');
                 end               
-            case 'Dentate'
+            case 'dentate'
                 if ha(3) == 0
-                    ha(3) = plot(units{a}.(feat1),units{a}.(feat2),'b*','DisplayName','Dentate');
+                    ha(3) = plot(units(a).(feat1),units(a).(feat2),'b*','DisplayName','Dentate');
                 else
-                    plot(units{a}.(feat1),units{a}.(feat2),'b*');
+                    plot(units(a).(feat1),units(a).(feat2),'b*');
                 end           
-            case 'Purkinje'
+            case 'purkinje'
                 if ha(4) == 0
-                    ha(4) = plot(units{a}.(feat1),units{a}.(feat2),'gd','DisplayName','Purkinje');
+                    ha(4) = plot(units(a).(feat1),units(a).(feat2),'gd','DisplayName','Purkinje');
                 else
-                    plot(units{a}.(feat1),units{a}.(feat2),'gd');
+                    plot(units(a).(feat1),units(a).(feat2),'gd');
                 end              
-            case 'Fiber'
+            case 'fiber'
                 if ha(5) == 0
-                    ha(5) =  plot(units{a}.(feat1),units{a}.(feat2),'m+','DisplayName','Fiber');
+                    ha(5) =  plot(units(a).(feat1),units(a).(feat2),'m+','DisplayName','Fiber');
                 else
-                    plot(units{a}.(feat1),units{a}.(feat2),'m+');
+                    plot(units(a).(feat1),units(a).(feat2),'m+');
+                end              
+            case 'ub'
+                if ha(6) == 0
+                    ha(6) =  plot(units(a).(feat1),units(a).(feat2),'m+','DisplayName','Unipolar Brush');
+                else
+                    plot(units(a).(feat1),units(a).(feat2),'m+');
+                end              
+            case 'basket'
+                if ha(7) == 0
+                    ha(7) =  plot(units(a).(feat1),units(a).(feat2),'m+','DisplayName','Basket');
+                else
+                    plot(units(a).(feat1),units(a).(feat2),'m+');
+                end              
+            case 'stellate'
+                if ha(8) == 0
+                    ha(8) =  plot(units(a).(feat1),units(a).(feat2),'m+','DisplayName','Stellate');
+                else
+                    plot(units(a).(feat1),units(a).(feat2),'m+');
+                end              
+            case 'granule'
+                if ha(9) == 0
+                    ha(9) =  plot(units(a).(feat1),units(a).(feat2),'m+','DisplayName','Granule');
+                else
+                    plot(units(a).(feat1),units(a).(feat2),'m+');
+                end              
+            case 'border'
+                if ha(10) == 0
+                    ha(10) =  plot(units(a).(feat1),units(a).(feat2),'m+','DisplayName','Border');
+                else
+                    plot(units(a).(feat1),units(a).(feat2),'m+');
                 end           
         end
         end
@@ -113,44 +178,74 @@ else
 end
 
 elseif nargin == 4
-if isprop(units{1,1},feat1)&&isprop(units{1,1},feat2)&&isprop(units{1,1},feat3)
+if isprop(units(1,1),feat1)&&isprop(units(1,1),feat2)&&isprop(units(1,1),feat3)
     figure();
     [r, c] = size(units);
     hold all;
     
-    ha = zeros(1,5);
+    ha = zeros(1,10);
     for a = 1:r
-        switch units{a}.label
+        switch units(a).label
             case 'unclassified'
                 if ha(1) == 0
-                    ha(1) = plot3(units{a}.(feat1),units{a}.(feat2),units{a}.(feat3),'ko','DisplayName','unclassified');
+                    ha(1) = plot3(units(a).(feat1),units(a).(feat2),units(a).(feat3),'ko','DisplayName','Unclassified');
                 else
-                    plot3(units{a}.(feat1),units{a}.(feat2),units{a}.(feat3),'ko');
+                    plot3(units(a).(feat1),units(a).(feat2),units(a).(feat3),'ko');
                 end
-            case 'Golgi'
+            case 'golgi'
                 if ha(2) == 0
-                    ha(2) = plot3(units{a}.(feat1),units{a}.(feat2),units{a}.(feat3),'rs','DisplayName','Golgi');
+                    ha(2) = plot3(units(a).(feat1),units(a).(feat2),units(a).(feat3),'rs','DisplayName','Golgi');
                 else
-                    plot3(units{a}.(feat1),units{a}.(feat2),units{a}.(feat3),'rs');
+                    plot3(units(a).(feat1),units(a).(feat2),units(a).(feat3),'rs');
                 end               
-            case 'Dentate'
+            case 'dentate'
                 if ha(3) == 0
-                    ha(3) = plot3(units{a}.(feat1),units{a}.(feat2),units{a}.(feat3),'b*','DisplayName','Dentate');
+                    ha(3) = plot3(units(a).(feat1),units(a).(feat2),units(a).(feat3),'b*','DisplayName','Dentate');
                 else
-                    plot3(units{a}.(feat1),units{a}.(feat2),units{a}.(feat3),'b*');
+                    plot3(units(a).(feat1),units(a).(feat2),units(a).(feat3),'b*');
                 end           
-            case 'Purkinje'
+            case 'purkinje'
                 if ha(4) == 0
-                    ha(4) = plot3(units{a}.(feat1),units{a}.(feat2),units{a}.(feat3),'gd','DisplayName','Purkinje');
+                    ha(4) = plot3(units(a).(feat1),units(a).(feat2),units(a).(feat3),'gd','DisplayName','Purkinje');
                 else
-                    plot(units{a}.(feat1),units{a}.(feat2),'gd');
+                    plot(units(a).(feat1),units(a).(feat2),'gd');
                 end              
-            case 'Fiber'
+            case 'fiber'
                 if ha(5) == 0
-                    ha(5) =  plot3(units{a}.(feat1),units{a}.(feat2),units{a}.(feat3),'m+','DisplayName','Fiber');
+                    ha(5) =  plot3(units(a).(feat1),units(a).(feat2),units(a).(feat3),'m+','DisplayName','Fiber');
                 else
-                    plot3(units{a}.(feat1),units{a}.(feat2),units{a}.(feat3),'m+');
-                end           
+                    plot3(units(a).(feat1),units(a).(feat2),units(a).(feat3),'m+');
+                end              
+            case 'ub'
+                if ha(6) == 0
+                    ha(6) =  plot3(units(a).(feat1),units(a).(feat2),units(a).(feat3),'m+','DisplayName','Unipolar Brush');
+                else
+                    plot3(units(a).(feat1),units(a).(feat2),units(a).(feat3),'m+');
+                end               
+            case 'basket'
+                if ha(7) == 0
+                    ha(7) =  plot3(units(a).(feat1),units(a).(feat2),units(a).(feat3),'m+','DisplayName','Basket');
+                else
+                    plot3(units(a).(feat1),units(a).(feat2),units(a).(feat3),'m+');
+                end               
+            case 'stellate'
+                if ha(8) == 0
+                    ha(8) =  plot3(units(a).(feat1),units(a).(feat2),units(a).(feat3),'m+','DisplayName','Stellate');
+                else
+                    plot3(units(a).(feat1),units(a).(feat2),units(a).(feat3),'m+');
+                end               
+            case 'granule'
+                if ha(9) == 0
+                    ha(9) =  plot3(units(a).(feat1),units(a).(feat2),units(a).(feat3),'m+','DisplayName','Granule');
+                else
+                    plot3(units(a).(feat1),units(a).(feat2),units(a).(feat3),'m+');
+                end               
+            case 'border'
+                if ha(10) == 0
+                    ha(10) =  plot3(units(a).(feat1),units(a).(feat2),units(a).(feat3),'m+','DisplayName','Border');
+                else
+                    plot3(units(a).(feat1),units(a).(feat2),units(a).(feat3),'m+');
+                end            
         end        
     end
     xlabel(feat1);
